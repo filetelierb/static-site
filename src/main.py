652ff0d4b0d8bd06f9ba8698440571565500b059
feletelier <felipe@igniuslab.com>
2025-09-18 13:20:44 +0200

    Fix basepath bug

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ import sys
 def main():
     copy_dir()
     
-    basepath = sys.argv[0] if len(sys.argv) > 0 else '/'
+    basepath = sys.argv[1] if len(sys.argv) > 1 else '/'
+    print(f"Basepath set to: {basepath}")
     generate_pages_recursive('./content', './template.html', './docs', basepath)
    
 main()
